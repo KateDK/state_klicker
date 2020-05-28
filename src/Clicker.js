486baf0ff2fa028d7e04e88527bfecdb3d483287
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-28 13:44:45 -0400

    Add state and render Number

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class Clicker extends React.Component{
+  state={
+    number:1,
+  };
   render(){
+    const {number} = this.state;
     return (
       <div className="Clicker">
-        Hello!
+        <h1>Number is: {number}</h1>
       </div>
     )
   }
