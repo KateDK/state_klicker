9657526bfee2709f3d1ede06b9ca89e97dd6d2aa
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-28 12:40:25 -0400

    Create and render basic Clicker component

---
+++ src/Clicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Clicker extends React.Component{
+  render(){
+    return (
+      <div className="Clicker">
+        Hello!
+      </div>
+    )
+  }
+}
+
+export default Clicker;
